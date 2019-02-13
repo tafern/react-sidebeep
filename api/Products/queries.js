6bced03714c711d01e09b8e4ecc1b4cd8bf23a04
Bye Webster <bayu@tafern.com>
2019-02-13 17:19:00 +0700

    Commit update product detail.

--- api/Products/queries.js
+++ api/Products/queries.js
@@ -1,15 +1,9 @@
 import Products from './Products';
 
 export default {
-  // orgProducts: ({ _id }) => Products.find({ orgId: _id }, { sort: { createdAt: 1 } }).fetch(),
-  products: () =>
-    // context.user && context.user._id ? Products.find({ owner: context.user._id }).fetch() : [],
-    Products.find({}),
-  product: (parent, args, context) =>
+  products: () => Products.find({}),
+  product: (parent, args) =>
     Products.findOne({
-      $or: [
-        { _id: args._id, owner: context.user && context.user._id ? context.user._id : null },
-        { _id: args._id, isPublic: true },
-      ],
+      $or: [{ _id: args._id }],
     }),
 };
