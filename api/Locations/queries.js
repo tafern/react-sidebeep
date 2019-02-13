4fafd08f3d8696e2271f5d4cedc8500ebd28b6ec
Bye Webster <bayu@tafern.com>
2019-02-13 20:23:32 +0700

    Commit Update Org.

--- api/Locations/queries.js
+++ api/Locations/queries.js
@@ -1,13 +1,9 @@
 import Locations from './Locations';
 
 export default {
-  locations: (parent, args, context) =>
-    context.user && context.user._id ? Locations.find({ owner: context.user._id }).fetch() : [],
-  location: (parent, args, context) =>
+  locations: ({ _id }) => Locations.find({ orgId: _id }, { sort: { createdAt: 1 } }).fetch(),
+  location: (parent, args) =>
     Locations.findOne({
-      $or: [
-        { _id: args._id, owner: context.user && context.user._id ? context.user._id : null },
-        { _id: args._id, isPublic: true },
-      ],
+      $or: [{ _id: args._id }],
     }),
 };
--- api/Orgs/queries.js
+++ api/Orgs/queries.js
@@ -1,9 +1,7 @@
 import Orgs from './Orgs';
 
 export default {
-  orgs: () =>
-    // context.user && context.user._id ? Orgs.find({ owner: context.user._id }).fetch() : [],
-    Orgs.find({}).fetch(),
+  orgs: () => Orgs.find({}).fetch(),
   org: (parent, args) =>
     Orgs.findOne({
       $or: [{ _id: args._id }],
